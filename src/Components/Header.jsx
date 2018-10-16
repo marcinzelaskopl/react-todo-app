1efcad68463acd0b3bacf7ef805fc6701838e3c3
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-10-16 12:48:39 +0200

    finished styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,10 @@ import './Header.css'
 class Header extends Component {
     render(){
         return (
-            <h1>React To-Do List</h1>
+            <div>
+                <h1>React To-Do List</h1>
+                <h3>You can click on ToDo item to change it color.</h3>
+            </div>
         )
     }
 }
