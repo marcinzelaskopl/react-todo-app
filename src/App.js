be681a642197d2b58b200e4af4c3c1035c945c49
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-10-16 11:53:17 +0200

    add todo functions

--- src/App.js
+++ src/App.js
@@ -4,12 +4,31 @@ import Header from "./Components/Header";
 import ToDoInput from "./Components/ToDoInput";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      ToDos: [
+          {id:0, text: 'Make dinner!'},
+          {id: 1, text: 'Laundry'},
+          {id: 2, text: 'Make a React App'}
+      ],
+        nextId: 3
+    }
+  }
+
+  addToDo = (toDoText) => {
+    console.log('Todo added', toDoText)
+};
+  removeToDo = (id) => {
+    console.log('ToDo removed,', id)
+  };
   render() {
     return (
       <div className="App">
         <div className="todo-wrapper">
           <Header/>
-          <ToDoInput/>
+          <ToDoInput todoText={''} addToDo={this.addToDo}/>
         </div>
       </div>
     );
