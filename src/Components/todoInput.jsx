7997829eff8c94da6786aac92a0145cd7c3216a1
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-10-16 11:34:37 +0200

    test handlers

--- src/Components/todoInput.jsx
+++ src/Components/todoInput.jsx
@@ -4,12 +4,27 @@ import './todoInput.css'
 class todoInput extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+
+        }
     }
+
+    handleChange = () => {
+      console.log('change here');
+    };
+
+    addToDo = todo => {
+      console.log('todo: ', todo);
+    };
     render(){
         return (
             <div>
-
+                <input type={'text'} value={''} onChange={this.handleChange}/>
+                <button onClick={() => this.addToDo(this.state.value)} className={'btn btn-main'}>Submit</button>
             </div>
         )
     }
 }
+
+export default todoInput
